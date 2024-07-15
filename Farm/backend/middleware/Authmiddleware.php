8e3a00ebf6518e8baa3f9df7d2c31e17e2ebef67
DCKAP <ashakdckap@gmail.com>
2024-07-15 15:35:53 +0530

    signup-login

---
+++ Farm/backend/middleware/Authmiddleware.php
@@ -0,0 +1,13 @@
+<?php
+
+// To check the auth users 
+class Authmiddleware{
+
+    public function handler(){
+        if (!isset($_SESSION['login'])) {
+            header('Location: /registration');
+        }
+    }
+
+
+}
